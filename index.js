53e40c929427e12dbcad57b78dd931874d944789
j3553pat <jessecodedev@gmail.com>
2023-01-26 00:52:00 -0600

    added click and drop feature

--- index.js
+++ index.js
@@ -7,42 +7,36 @@
 // * Why are you get a warning in your console? Fix it.
 // * Delete these comment lines!
 
-const stone = document.querySelectorAll('.stone')
-const rows = document.querySelectorAll("row")
-
-stone.addEventListener('dragStart',() => {
-    console.log("Stone is getting dragged")
+const stones = document.querySelectorAll('.stone')
+const rows = document.querySelectorAll(".row")
+stones.forEach(stone => {
+  stone.addEventListener('dragstart', () => {
+    stone.classList.add('dragging')
+    console.log("You Picked up a stone!")
   })
 
-// this function is called when a row is clicked. 
-// Open your inspector tool to see what is being captured and can be used.
-const selectRow = (row) => {
-  const currentRow = row.getAttribute("data-row")
-  console.log("Yay, we clicked an item", row)
-  console.log("Here is the stone's id: ", row.id)
-  console.log("Here is the stone's data-size: ", currentRow)
+  stone.addEventListener('dragend', () => {
+    stone.classList.remove('dragging')
+  })
+})
 
-  pickUpStone(row.id)
-} 
+rows.forEach(row => {
+  row.addEventListener('dragover', (e) => {
+    const stone = document.querySelector('.dragging')
+    e.preventDefault()
+    row.appendChild(stone)
+    console.log("the stone is being moved")
+  })
+})
 
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
-const pickUpStone = (rowID) => {
-  const selectedRow = document.getElementById(rowID);
-  stone = selectedRow.removeChild(selectedRow.lastChild);
-  console.log(stone)
-}
-
-// You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
-// Once you figure that out you'll need to figure out if its a legal move...
-// Something like: if(!stone){pickupStone} else{dropStone}
-
-const dropStone = (rowID, stone) => {
-  document.getElementById(rowID).appendChild(stone)
-  stone = null
-}
-
-const winningGame = () => {
-}
-// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
-
+const isLegal = (stones) => {
+  // Your code here
+  if (stones.length == 0) {
+    return true;
+  } else if (stones.at(-1) > stones.at(-1)) {
+    console.log("This is an illegal move.");
+    return false
+  } else {
+    return true;
+  }
+};
